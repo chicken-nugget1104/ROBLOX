dfbe8eea33705180d6e1ff3a02730865d6fb3227
chicken-nugget1104 <160080970+chicken-nugget1104@users.noreply.github.com>
2025-11-08 17:19:49 -0500

    TEST1

--- App/include/v8datamodel/PrismInstance.h
+++ App/include/v8datamodel/PrismInstance.h
@@ -13,56 +13,52 @@ namespace RBX {
 extern const char* const sPrism;
 
 class PrismInstance
-	: public DescribedNonCreatable<PrismInstance, PartInstance, sPrism>
+    : public DescribedCreatable<PrismInstance, PartInstance, sPrism>
 {
-	private:
-		typedef DescribedNonCreatable<PrismInstance, PartInstance, sPrism> Super;
+private:
+    typedef DescribedCreatable<PrismInstance, PartInstance, sPrism> Super;
 
-	public:
+public:
 
-		PrismInstance();
-		~PrismInstance();
+    PrismInstance();
+    ~PrismInstance();
 
-		enum NumSidesEnum
-		{
-			sides3 = 3, 
-			sides4	 ,
-			sides5	 ,
-			sides6	 ,
-			sides7	 ,
-			sides8	 ,
-			sides9	 ,
-			sides10 ,
-			sides11 ,
-			sides12 ,
-			sides13 ,
-			sides14 ,
-			sides15 ,
-			sides16 ,
-			sides17 ,
-			sides18 ,
-			sides19 ,
-			sides20 ,
-		};
+    enum NumSidesEnum
+    {
+        sides3 = 3, 
+        sides4,
+        sides5,
+        sides6,
+        sides7,
+        sides8,
+        sides9,
+        sides10,
+        sides11,
+        sides12,
+        sides13,
+        sides14,
+        sides15,
+        sides16,
+        sides17,
+        sides18,
+        sides19,
+        sides20,
+    };
 
-		static const Reflection::EnumPropDescriptor<PrismInstance, NumSidesEnum> prop_sidesXML;
+    static const Reflection::EnumPropDescriptor<PrismInstance, NumSidesEnum> prop_sidesXML;
 
-		void SetNumSides(const PrismInstance::NumSidesEnum num);
-		NumSidesEnum GetNumSides() const;
+    void SetNumSides(const PrismInstance::NumSidesEnum num);
+    NumSidesEnum GetNumSides() const;
 
-		void SetNumSlices(const int num);
-		int GetNumSlices() const;
-	 
-		/*override*/ virtual PartType getPartType() const { return PRISM_PART; }
-		/*override*/ virtual void setPartSizeXml(const Vector3& rbxSize);
+    void SetNumSlices(const int num);
+    int GetNumSlices() const;
+ 
+    /*override*/ virtual PartType getPartType() const { return PRISM_PART; }
+    /*override*/ virtual void setPartSizeXml(const Vector3& rbxSize);
 
-	private:
-		// These are redundant with data in the Poly
-
-		// shape parameters
-		//NumSidesEnum NumSides;
-		//int NumSlices;
-
+private:
+    NumSidesEnum numSides;
+    int numSlices;
 
 };
 
